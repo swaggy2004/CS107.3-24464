21285ac140896e2a3ac950334b8e18a79c19e735
Swaggy <101066701+swaggy2004@users.noreply.github.com>
2023-06-09 16:48:00 +0530

    [Lab 4 all questions completed]

---
+++ Lab 4/Lab4-2/FindValues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab4_2
+{
+    internal class FindValues
+    {
+        public double FindArea(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+        public double FindCircumference(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+    }
+}
---
+++ Lab 4/Lab4-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            FindValues ObjCircle = new FindValues();
+            Console.WriteLine("Area: {0}", ObjCircle.FindArea(radius));
+            Console.WriteLine("Circumference: {0}", ObjCircle.FindCircumference(radius));
+            Console.ReadKey();
+        }
+    }
+}
