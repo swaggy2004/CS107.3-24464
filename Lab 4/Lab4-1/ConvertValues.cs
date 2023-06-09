59fe8e3d38018f047624d0678507411812fc6438
Swaggy <101066701+swaggy2004@users.noreply.github.com>
2023-06-09 16:37:24 +0530

    [Lab 4 answers]

---
+++ Lab 4/Lab4-1/ConvertValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab4_1
+{
+    internal class ConvertValues
+    {
+        public void killometerTOmeter()
+        {
+            Console.Write("Enter Killometer value: ");
+            double killometer = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The value is meter is {killometer * 1000}");
+        }
+
+        public void killometerTOmeter(double d)
+        {
+            Console.WriteLine($"The value is meter is {d * 1000}");
+        }
+
+        public float killometerTOmeter(float distance)
+        {
+            return distance * 1000;
+        }
+    }
+}
---
+++ Lab 4/Lab4-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // No parameter and no return type
+            ConvertValues ObjValue = new ConvertValues();
+            ObjValue.killometerTOmeter();
+
+            // With parameter and no return type
+            Console.WriteLine("This is outside the method!");
+            Console.Write("Enter distance is Kilometer: ");
+            double distance = Convert.ToDouble(Console.ReadLine());
+            ObjValue.killometerTOmeter(distance);
+
+            //  With parameter and with return type
+            Console.Write("Enter distance is Kilometer: ");
+            float distance1 = float.Parse(Console.ReadLine());
+            Console.WriteLine($"This is the value in {ObjValue.killometerTOmeter(distance1)}");
+            Console.ReadLine();
+
+        }
+    }
+}
