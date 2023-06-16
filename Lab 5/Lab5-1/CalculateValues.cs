ab0b9cc83601c895bcfb378ae6952af5ed8f87e5
Shaveen <shaveenb2004@outlook.com>
2023-06-16 16:34:26 +0530

    Updated version of Lab 6

---
+++ Lab 5/Lab5-1/CalculateValues.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_1
+{
+    internal class CalculateValues
+    {
+        public float Add(float a, float b)
+        {
+            return a + b;
+;       }
+        public float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+        public float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+        public float Divide(float a, float b)
+        { 
+            return a / b;
+        }
+    }
+}
